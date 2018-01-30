2ef7c9e9da0a3982c59e044177a5b2de06ddfef6
tzine <34639191+tzine@users.noreply.github.com>
2018-01-29 22:42:04 -0500

    GreetPerson
    week2 assignment

---
+++ src/com/company/GreetPerson.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class GreetPerson {
+
+    public static void main(String[] args) {
+	String person = "Martha";
+	System.out.println("It's your lucky day, " + person + "!");
+
+
+    }
+}
